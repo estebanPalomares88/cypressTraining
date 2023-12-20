700f0aef60b8926ac138cae39039451d12aa88b5
karol cardenas <karolcardenas@karols-MacBook-Air.local>
2023-12-19 19:37:00 -0500

    POM Created, first scenario started

--- cypress/e2e/ui/login.cy.js
+++ cypress/e2e/ui/login.cy.js
@@ -1,5 +1,21 @@
-describe('Login in gmail page', () => {
-  it('passes', () => {
+import { LoginPage } from "../main/E2E/pom/LoginPage";
+
+describe('Login scenarios in gmail page',() => {
+
+  var loginPage = new LoginPage();
+
+  beforeEach(() => {
     cy.visit('https://www.gmail.com')
+  })
+
+  it('Succes Login', () => {
+    cy.get(loginPage.userInput)
+    .type('testUser8892@gmail.com')
+
+    cy.get(loginPage.nextButton)
+    .click()
+
+    cy.get(loginPage.passInput,{timeout: 10000})
+    //.type('Testing.2023')
   })
 })
---
+++ cypress/main/E2E/pom/LoginPage.js
@@ -0,0 +1,8 @@
+export class LoginPage {
+
+    constructor(){
+        this.userInput = '#identifierId'
+        this.nextButton = '#identifierNext'
+        this.passInput = '#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input'
+    }
+}
