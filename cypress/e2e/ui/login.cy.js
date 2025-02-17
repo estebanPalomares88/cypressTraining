724a5138e3f258bb6e8c6f01ebc0df80d62da4d8
estebanPalomares88 <154276638+estebanPalomares88@users.noreply.github.com>
2025-02-17 12:37:01 -0500

    UI elements added (#3)
    * POM Created, first scenario started
    
    * project migrated
    
    * login scenario updated
    
    ---------
    
    Co-authored-by: karol cardenas <karolcardenas@karols-MacBook-Air.local>

--- cypress/e2e/ui/login.cy.js
+++ cypress/e2e/ui/login.cy.js
@@ -1,5 +1,18 @@
-describe('Login in gmail page', () => {
-  it('passes', () => {
-    cy.visit('https://www.gmail.com')
-  })
-})
+import { LoginPage } from "./pom/LoginPage";
+
+describe('Login scenarios', () => {
+  let loginPage;
+
+  beforeEach(() => {
+    loginPage = new LoginPage();
+    cy.visit('https://www.saucedemo.com/v1/index.html');
+    
+  });
+
+  it('Successful Login', () => {
+    cy.get(loginPage.userInput).type('standard_user');
+    cy.get(loginPage.passInput).type('secret_sauce');
+    cy.get(loginPage.loginButton).click();
+    cy.title().should('eq', 'Swag Labs');
+  });
+});
