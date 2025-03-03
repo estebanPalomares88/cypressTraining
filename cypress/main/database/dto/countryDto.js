97ff2315f58ff8363c6e223738f360f3020585b0
Jaime Palomares <estebanpalomares23@gmail.com>
2025-03-03 12:54:21 -0500

    nested quieries scenario added

---
+++ cypress/main/database/dto/countryDto.js
@@ -0,0 +1,19 @@
+export class countryDto {
+    constructor(code, name, continent, region, surfaceArea, indepYear, population, lifeExpectancy, gnp, gnpOld, localName, governmentForm, headOfState, capital, code2) {
+        this.code = code;
+        this.name = name;
+        this.continent = continent;
+        this.region = region;
+        this.surfaceArea = surfaceArea;
+        this.indepYear = indepYear;
+        this.population = population;
+        this.lifeExpectancy = lifeExpectancy;
+        this.gnp = gnp;
+        this.gnpOld = gnpOld;
+        this.localName = localName;
+        this.governmentForm = governmentForm;
+        this.headOfState = headOfState;
+        this.capital = capital;
+        this.code2 = code2;
+    }
+}
